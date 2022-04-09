24a4c66814317c13dd999789d28469652aaed217
Hongbo He <hhbcarl@gmail.com>
2022-04-09 21:33:09 +0800

    Add bookstore

---
+++ bookstore/store/store.go
@@ -0,0 +1,14 @@
+type Book struct {
+	Id     string   `json:"id"`
+	Name   string   `json:"name"`
+	Author []string `json:"authors"`
+	Press  string   `json:"press"`
+}
+
+type Store interface {
+	Create(*Book) error
+	Update(*Book) error
+	Get(string) (Book, error)
+	GetAll() ([]Book, error)
+	Delete(string) error
+}
